179ee87a083b26bd1167b5162145fbecdca7b884
Adrian Mihai Nutiu <adrian@nutiu.com>
2012-11-16 06:19:00 +0200

    Add validation to models

--- app/controllers/issues_controller.rb
+++ app/controllers/issues_controller.rb
@@ -2,7 +2,10 @@ class IssuesController < ApplicationController
   # GET /issues
   # GET /issues.json
   def index
-    @issues = Issue.all
+    @issues = Issue
+    @issues = Issue.order(params[:sort]) if params[:sort]
+   
+    @issues = @issues.all
 
     respond_to do |format|
       format.html # index.html.erb
--- app/models/issue.rb
+++ app/models/issue.rb
@@ -1,3 +1,8 @@
 class Issue < ActiveRecord::Base
   attr_accessible :description, :no_followers, :title
+
+  validates :title, presence: :true , length: {minimum: 5}
+  validates :description, presence: :true
+  
+  
 end
